42399ca77201614e5630ebc087e654758831f3d2
Richard Hanks <richardhanksjr@gmail.com>
2018-12-28 23:02:24 -0500

    Hello world working

--- music_study_app/api/urls.py
+++ music_study_app/api/urls.py
@@ -1,1 +1,6 @@
-pass
+from django.urls import path
+from api.views import TestView
+
+urlpatterns = [
+	path('', TestView.as_view(), name='home'),
+]
--- music_study_app/api/views.py
+++ music_study_app/api/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# Create your views here.
+class TestView(APIView):
+
+    def get(self, request, format=None):
+        return Response("Hello World")
