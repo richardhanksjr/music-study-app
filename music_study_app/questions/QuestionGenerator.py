c231c1b0dbc09fcb8dc4dee79f61fd8b43445dd9
Richard Hanks <richardhanksjr@gmail.com>
2019-01-29 20:52:22 -0500

    Building out the QuestionGenerator.

--- music_study_app/questions/QuestionGenerator.py
+++ music_study_app/questions/QuestionGenerator.py
@@ -3,6 +3,12 @@ from .questions import Question
 
 class QuestionGenerator:
 
+    question_choices = {'simple-interval-major': SimpleIntervalMajor}
+
     @staticmethod
-    def random_question():
+    def question_factory(question_type=None):
+        if not question_type:
+            raise TypeError('question_factory missing keyword argument question_type')
+
+        # TODO get question based on the question_type and the question_choices dict above
         return Question()
--- music_study_app/questions/test_questions.py
+++ music_study_app/questions/test_questions.py
@@ -5,6 +5,15 @@ from .questions import Question
 
 class QuestionGeneratorTest(TestCase):
 
-    def test_that_generator_returns_an_instance_of_question(self):
-        question = QuestionGenerator.random_question()
-        self.assertIsInstance(question, Question)
+    def test_question_factory_raises_type_error_if_question_type_missing(self):
+        try:
+            QuestionGenerator.question_factory()
+            self.assertTrue(False)
+        except TypeError:
+            self.assertTrue(True)
+        except Exception:
+            self.assertFalse(False)
+
+    # def test_that_generator_returns_an_instance_of_question(self):
+    #     question = QuestionGenerator.question_factory()
+    #     self.assertIsInstance(question, Question)
