12123bed3e47250a672c42ee99b218c578a62944
Richard Hanks <richardhanksjr@gmail.com>
2019-03-22 21:55:51 -0400

    Working question and answer cycle.

--- music_study_app/application/static/js/app.js
+++ music_study_app/application/static/js/app.js
@@ -3,7 +3,8 @@ var question_params;
 
 function populateQuestion(data){
     const answer_letters = ["a", "b", "c", "d"]
-    console.log(data)
+    $("#new_question").hide();
+    $("#submit_answer").show();
     $("#question").html(data.question);
     $("#answer_options").empty();
     data.answer_options.forEach((answer, index) => {
@@ -19,14 +20,27 @@ function getNewQuestion(){
     });
 }
 
+function displayAnswer(data){
+  $("#new_question").show();
+  $("#submit_answer").hide();
+  $("#question").empty();
+  $("#answer_options").empty();
+  if(data.user_correct){
+      $("#answer_display").html("You are correct");
+  }else{
+      $("#answer_display").html("You are incorrect");
+  }
+}
+
 function submitAnswer(){
     const user_answer = $('input[name=answer]:checked').val();
     const payload = Object.assign(question_params, {user_answer})
-    console.log(payload)
     $.post(`${api_url}/answer`, payload)
-        .done((data) => console.log(data))
+        .done((data) => displayAnswer(data))
 
 }
+
+
 
 
 $('#new_question').click(function(){
